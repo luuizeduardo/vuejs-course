b9cae5492798e132a157e3de92ca7a0ec7101ff9
Luiz Eduardo <led.martins93@gmail.com>
2018-04-04 23:01:20 -0300

    Continuação do curso

--- js/exemplo3.js
+++ js/exemplo3.js
@@ -1,6 +1,14 @@
 new Vue({
 	el: '#app',
     data:{
-  	    link: 'http://google.com'
+        title: 'Título da aplicação',
+        link: 'http://google.com',
+        linkCompleto: '<a href="http://google.com">Google</a>'
+    },
+    methods: {
+        sayHello: function(){
+            this.title = 'Hey! The title has changed';
+            return this.title;
+        }
     }
 });
